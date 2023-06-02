971f1a429b7ada3743e46bd76e231972a7a095bd
dariwan <dariwan1607@gmail.com>
2023-06-02 19:10:28 +0700

    adding isFirstTimeLaunch using shared preferences

--- app/src/main/java/com/example/pantrypal/view/getstarted/GetStartedActivity.kt
+++ app/src/main/java/com/example/pantrypal/view/getstarted/GetStartedActivity.kt
@@ -1,15 +1,16 @@
 package com.example.pantrypal.view.getstarted
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.pantrypal.R
 import com.example.pantrypal.databinding.ActivityGetStartedBinding
 import com.example.pantrypal.view.main.MainActivity
 
 class GetStartedActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGetStartedBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +19,40 @@ class GetStartedActivity : AppCompatActivity() {
 
 
         binding.buttonGetStarted.setOnClickListener {
+            isFirstTimeLaunch()
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
+
+
+    //first time enter aplikasi
+    private fun isFirstTimeLaunch(){
+        val sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.apply{
+            putBoolean(FIRST_TIME_LAUNCH, true)
+        }.apply()
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        val sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val isFirstTimeLaunch = sharedPreferences.getBoolean(FIRST_TIME_LAUNCH, false)
+        if (isFirstTimeLaunch){
+            val intentToMain = Intent(this@GetStartedActivity, MainActivity::class.java)
+            startActivity(intentToMain)
+            finish()
+        }
+    }
+
+
+    companion object{
+        const val PREFS_NAME = "pantrypall_app_pref"
+        const val FIRST_TIME_LAUNCH = "first_time_launch"
+    }
+
+
+
 }
